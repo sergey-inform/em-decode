4cb5cd72c3427820519eedb6a95588637e04b25e
Sergey Ryzhikov <sergey-inform@ya.ru>
2017-11-22 15:10:57 +0300

    infiles -> infile

--- src/em-parse.c
+++ src/em-parse.c
@@ -18,24 +18,27 @@
 
 const char *argp_program_version = "em-parse 2.0";
 const char *argp_program_bug_address = "\"Sergey Ryzhikov\" <sergey-inform@ya.ru>";
-static char doc[] = "Parse EuroMISS raw data, output valid event data.\n" \
-	"Use --verbose and --debug flags to understand what is going on." \
-	"\v Text after opts.";
-static char args_doc[] = "CrateID [FILENAMES...]";
+static char doc[] = "Parse EuroMISS raw data file, output valid em-event structures.\n" \
+	"\v How to use:\n" \
+	"Use --verbose and --debug flags to understand what's going on." \
+	"...\n.";  // FIXME
+static char args_doc[] = "CrateID [FILENAME]";
 
 static struct argp_option options[] = { 
-	{0,0,0,0, "CrateID is an integer number." },
+	{0,0,0,0, "CrateID is an integer number, decimal or hex with '0x' prefix." },
+	{0,0,0,0, "If no FILENAME, wait for data in stdin." },
+	{0,0,0,0, "Options:" },
 	{ "debug", 'd', 0, 0, "Interprete input word by word."},
 	{ "verbose", 'v', 0, 0, "Trace all events to stderr."},
 	{ "quiet", 'q', 0, 0, "Print to stderr only errors."},
 	{ "stats", 's', 0, 0, "Print statistics."},
-	{ "output", 'o', "OUTFILE", 0, "Output events to OUTFILE instead of stdout."},
+	{ "output", 'o', "OUTFILE", 0, "Instead of stdout output events to OUTFILE."},
 	{ 0 } 
 };
 
 struct args {
 	bool debug, verbose, quiet, stats;
-	char **infiles;
+	char *infile;
 	char *outfile;
 	unsigned crate_id;
 	bool no_output;
@@ -59,22 +62,24 @@ parse_opt(int key, char *arg, struct argp_state *state)
 			argp_usage(state);
 
 		case ARGP_KEY_ARG: 
-			if (state->arg_num >= 2){
-				argp_usage(state);
-			}
-			crate_id = strtoul(arg, NULL, 0 /*base*/);
+			if (state->arg_num == 0) {  // CrateID
+				crate_id = strtoul(arg, NULL, 0 /*base*/);
 
-			// check CrateID
-			if (crate_id < 0) {
-				argp_failure(state, EX_USAGE, EINVAL,
-					"CrateID should be unsigned integer"\
-					", but '%s' is given.",	arg);
+				// check CrateID is valid
+				if (crate_id < 0) {
+					argp_failure(state, EX_USAGE, EINVAL,
+						"CrateID should be unsigned integer"\
+						", but '%s' is given.",	arg);
+				}
+				args->crate_id = (unsigned int)crate_id;
+			
 			}
-			args->crate_id = (unsigned int)crate_id;
-
-			// consume the rest of arguments as filenames
-			args->infiles = &state->argv[state->next];
-			state->next = state->argc;
+			else if (state->arg_num == 1) {  // FILENAME
+				args->infile = arg;
+			}
+			else {
+				argp_usage(state); // more then two args is a typo
+			}
 			break;
 
 		default: return ARGP_ERR_UNKNOWN;
@@ -92,9 +97,11 @@ void dump_args( struct args * args)
 {
 	int j;
 	printf(	"outfile %s \n" \
+		"infile %s \n" \
 		"CrateId %d \n" \
 		"flags: %s%s%s%s \n",
 		args->outfile,
+		args->infile,
 		args->crate_id,
 		args->verbose ? "verbose ": "",
 		args->stats ? "stats ": "",
@@ -102,16 +109,13 @@ void dump_args( struct args * args)
 		args->quiet ? "quiet ": ""
 		);
 
-	printf("infiles: ");
-	for (j = 0; args->infiles[j]; j++)
-		printf(j==0 ? "%s" : ", %s", args->infiles[j]);
 	printf("\n");
 	fflush(stdout);
 
 }
 
 
-int check_infiles( char ** infiles)
+int check_infile( char * infile)
 /** Check input files exists and readable */
 {
 
@@ -122,20 +126,20 @@ int check_infiles( char ** infiles)
 int main(int argc, char *argv[])
 {
 	struct args args = {0};
-	FILE * const outfile;
-	FILE ** const infiles;
+	FILE * outfile;
+	FILE * const infile;
 
 	// Defaults
 	args.outfile = "-";
-	args.infiles = (char *[]) {"-", NULL};	
+	args.infile = "-";	
 	
 	argp_parse(&argp, argc, argv, 0, 0, &args);
-	//dump_args(&args);
+	dump_args(&args);
 	
 	if ( !strcmp(args.outfile, "-") ) { // output to stdout
 		if (isatty(fileno(stdout))) { // stdout is a tty
 			args.no_output = true;
-			fprintf(stderr, "Binary output to terminal, srsly? Output suppressed.\n")
+			fprintf(stderr, "Binary output to terminal, srsly? Output suppressed.\n");
 		}
 		else {
 			outfile = fdopen(dup(fileno(stdout)), "wb"); // force binary output ...
@@ -143,7 +147,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if ( check_infiles(args.infiles)) {
+	if ( check_infile(args.infile)) {
 		
 	}
 		

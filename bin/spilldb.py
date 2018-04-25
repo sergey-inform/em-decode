4452d0b5dbff3a3a225c192ff4e35e31f1af020c
Sergey Ryzhikov <sergey-inform@ya.ru>
2018-04-25 22:26:06 +0300

    walk spills

--- bin/spilldb.py
+++ bin/spilldb.py
@@ -23,13 +23,56 @@ def allspills(rootdir, ext=".dat.gz"):
     from glob import iglob
     from os.path import basename
 
-    spills = set()
+    spills = []
     globpath = str(rootdir) + "/**/*" + str(ext)
     
     for filename in iglob(globpath, recursive=True):
         fn = basename(filename)[:-len(ext)] 
                     # rootdir/sub/1521331424.6.dat.gz -> '1521331424.6' 
         ts = int(fn[:10])  # '1521331424.6' -> 1521331424
-        spills.add(ts)
+        spills.append(ts)
 
+    #TODO: -> os.walk
+
+    return set(spills)
+    
+def walkspills(rootdir, ext=".dat.gz"):
+    """
+
+    """
+    from os import walk
+
+    spills = []
+
+    def handlerr(err):
+        raise err
+
+    def name2ts(filename, ext=".dat.gz"):
+        """
+        '1521331424.6.dat.gz' -> 1521331424
+        Return None if file doesn't match extension or can not be converted.
+        """
+        if str(filename).endswith(ext):
+            try:
+                return int((filename[:-len(ext)])[:10])
+            except ValueError:
+                return None
+        else:
+            return None
+
+    #TODO: support globs
+    for root, dirs, files in walk(rootdir,
+                topdown=True, followlinks=True, onerror=handlerr):
+        dirs.sort()
+        timestamps = set([name2ts(_, ext) for _ in files])
+        try:
+            timestamps.remove(None)
+        except KeyError:
+            pass
+
+        tslist = sorted(timestamps)
+        if tslist:
+            spills.extend(tslist)
+    
     return spills
+

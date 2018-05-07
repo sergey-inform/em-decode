c7b3d2f3bfc9eb6fa90ef5730d9240dc52f7f762
Sergey Ryzhikov <sergey-inform@ya.ru>
2018-05-07 21:33:03 +0300

    debug script

---
+++ src/plot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+
+filename = sys.argv[1]
+
+vals = np.loadtxt(filename, dtype=int)
+svals = sorted(vals)
+
+c,b, p = plt.hist(svals, bins='fd', normed=False, range=(0,50000) )
+
+plt.show()
+
+
